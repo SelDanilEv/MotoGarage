b1200a6feaf4544beb5b2450cd7e57809a0e7819
Danil <75370668+danilselitskiy@users.noreply.github.com>
2021-12-07 12:37:09 +0300

    Add user management page

---
+++ src/Infrastructure/Models/User/UserEditModel.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models.CommonModels;
+
+
+namespace Infrastructure.Models.User
+{
+    public class UserEditModel : UserModel
+    {
+        public string Role { get; set; }
+    }
+}
