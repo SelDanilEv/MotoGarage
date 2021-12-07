5f39b89719b72886ef976a1bd38985a2bc705654
Danil <75370668+danilselitskiy@users.noreply.github.com>
2021-12-07 12:37:09 +0300

    Add user management page

---
+++ src/Infrastructure/Dto/User/UserEditDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dto.User
+{
+    public class UserEditDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
